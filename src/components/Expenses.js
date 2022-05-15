ae10657fbbcef2d4bbc794c2e7be579f2c7d5148
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-05-15 17:56:13 +0100

    ract challenge to create a expenses component that hold all the expencesItem component

---
+++ src/components/Expenses.js
@@ -0,0 +1,33 @@
+import ExpenceItem from "./ExpenceItem";
+import "./Expenses.css"
+
+function Expenses(props){
+  console.log(props.allExpenses);
+  return(
+    <div className="expenses">
+      <ExpenceItem 
+          title={props.allExpenses[0].title}
+          amount={props.allExpenses[0].amount}
+          date={props.allExpenses[0].date}
+      ></ExpenceItem>  
+      <ExpenceItem
+          title={props.allExpenses[1].title}
+          amount={props.allExpenses[1].amount}
+          date={props.allExpenses[1].date}
+      ></ExpenceItem>     
+      <ExpenceItem
+          title={props.allExpenses[2].title}
+          amount={props.allExpenses[2].amount}
+          date={props.allExpenses[2].date}
+      ></ExpenceItem>     
+      <ExpenceItem
+          title={props.allExpenses[3].title}
+          amount={props.allExpenses[3].amount}
+          date={props.allExpenses[3].date}
+      ></ExpenceItem> 
+    </div>
+  );
+}
+
+
+export default Expenses
